e522796225ddc00ca19a6dba77de556a1ee97355
Edson <jredson525@gmail.com>
2025-09-13 11:49:14 -0300

    feat:adicionandoviews-e-templates

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
-from .views import HelloView, LivroListView, LivroCreateView, LivroUpdateView, LivroDeleteView
+from .views import HelloView, LivroListView, LivroCreateView, LivroUpdateView, LivroDeleteView, AutorListView, AutorCreateView, AutorUpdateView, AutorDeleteView, UsuarioListView, UsuarioCreateView, UsuarioUpdateView, UsuarioDeleteView
 
 urlpatterns = [path("", HelloView.as_view(), name="index"),
                path("listar", LivroListView.as_view(), name="listar_livros"),
                path("adicionar", LivroCreateView.as_view(), name="adicionar_livro"),
                path("atualizar/<int:pk>", LivroUpdateView.as_view(), name="atualizar_livro"),
                path("deletar/<int:pk>", LivroDeleteView.as_view(), name="deletar_livro"),
+               path("listarAut", AutorListView.as_view(), name="listar_autores"),
+               path("adicionarAut", AutorCreateView.as_view(), name="adicionar_autor"),
+               path("atualizarAut/<int:pk>", AutorUpdateView.as_view(), name="atualizar_autor"),
+               path("deletarAut/<int:pk>", AutorDeleteView.as_view(), name="deletar_autor"),
+               path("listarUsu", UsuarioListView.as_view(), name="listar_usuarios"),
+               path("adicionarUsu", UsuarioCreateView.as_view(), name="adicionar_usuario"),
+               path("atualizarUsu/<int:pk>", UsuarioUpdateView.as_view(), name="atualizar_usuario"),
+               path("deletarUsu/<int:pk>", UsuarioDeleteView.as_view(), name="deletar_usuario"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Livro, Autor, Usuario
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
-from .forms import LivroForm
+from .forms import LivroForm, AutorForm, UsuarioForm
 from django.urls import reverse_lazy
 from django.views import View
 from django.http import HttpResponse
@@ -35,6 +35,60 @@ class LivroDeleteView(DeleteView):
     template_name = "deletarlivro.html"
     context_object_name = ("livro")
     success_url = reverse_lazy("listar_livros")
+
+
+
+
+
+class AutorListView(ListView):
+    model = Autor
+    template_name = "list_autor.html"
+    context_object_name = "autores"
+
+class AutorCreateView(CreateView):
+    model = Autor
+    form_class = AutorForm
+    template_name = "adicionarautor.html"
+    success_url = reverse_lazy("listar_autores")
+
+class AutorUpdateView(UpdateView):
+    model = Autor
+    form_class = AutorForm
+    template_name = "adicionarautor.html"
+    context_object_name = ("autor")
+    success_url = reverse_lazy("listar_autores")
+
+class AutorDeleteView(DeleteView):
+    model = Autor
+    template_name = "deletarautor.html"
+    context_object_name = ("autor")
+    success_url = reverse_lazy("listar_autores")
+
+
+class UsuarioListView(ListView):
+    model = Usuario
+    template_name = "list_usuario.html"
+    context_object_name = "usuarios"
+
+class UsuarioCreateView(CreateView):
+    model = Usuario
+    form_class = UsuarioForm
+    template_name = "adicionarusuario.html"
+    success_url = reverse_lazy("listar_usuarios")
+
+class UsuarioUpdateView(UpdateView):
+    model = Usuario
+    form_class = UsuarioForm
+    template_name = "adicionarusuario.html"
+    context_object_name = ("usuario")
+    success_url = reverse_lazy("listar_usuarios")
+
+class UsuarioDeleteView(DeleteView):
+    model = Usuario
+    template_name = "deletarusuario.html"
+    context_object_name = ("usuario")
+    success_url = reverse_lazy("listar_usuarios")
+
 
 
 
