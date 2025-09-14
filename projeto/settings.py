77fc567a7cd82d25f6c5e0424aca5cb43ea88f80
Edson <jredson525@gmail.com>
2025-09-13 21:26:27 -0300

    feat: Instalação de um banco de dados externo (MySQL)

--- projeto/settings.py
+++ projeto/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'projeto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'meuBanco',
+        'USER': 'edson',
+        'PASSWORD': '15975325jr.',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
